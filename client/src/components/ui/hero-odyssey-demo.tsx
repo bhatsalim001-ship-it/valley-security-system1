5286342572df362f8625cc26902f3a1c26835f1f
Salim Bhat <vllscrtservice@gmail.com>
2026-06-22 23:17:23 +0530

    Integrate Hero Odyssey component and add premium light mode background

---
+++ client/src/components/ui/hero-odyssey-demo.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from "./hero-odyssey";
+
+const DemoOne = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <HeroSection />
+    </div>
+  );
+};
+
+export { DemoOne };
